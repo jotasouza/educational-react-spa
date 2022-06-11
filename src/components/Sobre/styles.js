8d1c0921a2d35d9cf72d0b7e43b13c37d21b17c1
JotaSouza <jotabccif@gmail.com>
2022-06-10 21:57:04 -0300

    feat: criado componente Sobre

---
+++ src/components/Sobre/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const ImgDiv = styled.div`
+  img {
+    border-radius: 15px;
+  }
+`;
+
+export const ConteudoDiv = styled.div`
+  padding-left: 50px;
+
+  h2 {
+    font-weight: 600;
+  }
+`;
+
+export const ContadorTitle = styled.p`
+  font-size: 1rem;
+  color: #0a2b1e;
+  font-weight: 500;
+`;
+
+export const ContadorSpan = styled.span`
+  font-size: 2rem;
+  font-weight: 600;
+  color: #0a2b1e;
+`;
